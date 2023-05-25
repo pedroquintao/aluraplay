11189b3bf9565399a103392adbe5abd4a39a919d
pedroquintao <pedro.h.quintao@hotmail.com>
2023-05-25 13:04:10 -0300

    Finalizado o conteúdo da aula: Requisição POST

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,6 +5,30 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            //"Content-type": é utilizado para especificar qual tipo de arquivo está sendo enviado ou recebido
+            "Content-type": "application/json"
+        },
+        //body: é utilizado para especificar os dados que se quer cadastrar na requisição, é o "conteúdo da mensagem"
+        //JSON.srtringfy(x) converte toda a informação x em string
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem 
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+//O conteúdo dentro desse export pode ser utilizado em outros arquivos.
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
